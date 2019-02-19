835b578ba84a351a2443da9945484216a0e932fe
Osama <osama.qalam@hotmail.com>
2019-02-18 22:02:55 -0500

    Created new files and achieved basic schedule of two courses

--- DBInterface.php
+++ DBInterface.php
@@ -6,9 +6,10 @@ function getLectureSections($course)
 {
   if ($course == "COMP232")
   {
-    $comp232_L2 = new Session (2,"COMP232", "PP", null, "F", array("T"), 1745, 2015, "SGW");
+    $comp232_L2 = new Session (2,"COMP232", "PP", null, "F", array("J"), 1745, 2015, "SGW");
+    $comp232_L1 = new Session (2,"COMP232", "PS", null, "F", array("M"), 1745, 2015, "SGW");
 
-    $sec = array ($comp232_L2);
+    $sec = array ($comp232_L2, $comp232_L1);
     return  $sec;
   }
   elseif ($course == "COMP248")
@@ -17,24 +18,51 @@ function getLectureSections($course)
     $comp248_L1 = new Session (3,"COMP248", "EE", null, "F", array("J"), 1745, 2015, "SGW");
 
     ////////////////////////
-    $comp248_L2 = new Session (3,"COMP248", "P", null, "F", array("M","W"), 1315, 1430, "SGW");
+    //$comp248_L2 = new Session (3,"COMP248", "P", null, "F", array("T","W"), 1745, 2015, "SGW");
 
-    $sec = array ($comp248_L1, $comp248_L2);
+    $sec = array ($comp248_L1);
     return  $sec;
   }
 }
 
 //Returns an array of Sessions objects available tutorials for this specific lecture
 //$lecture and $section are of type string, lecture name and section.
-function getTutorials($lecture, $section)
+function getTutorials($course, $section)
 {
+  if ($course == "COMP232" and $section == "PP")
+  {
+    $comp232_T1 = new Session (2,"COMP232", $section, "PA", "F", array("T"), 2030, 2130, "SGW");
 
+    $sec = array ($comp232_T1);
+    return  $sec;
+  }
+  elseif ($course == "COMP232" and $section == "PS") {
+    $comp232_T2 = new Session (2,"COMP232", $section, "SA", "F", array("S"), 2030, 2130, "SGW");
+    $sec = array ($comp232_T2);
+    return  $sec;
+  }
+  elseif ($course == "COMP248")
+  {
+
+    $comp248_T1 = new Session (3,"COMP248", $section, "EA", "F", array("F"), 1745, 2015, "SGW");
+    $comp248_T2 = new Session (3,"COMP248", $section, "EB", "F", array("J"), 2030, 2130, "SGW");
+
+    $sec = array ($comp248_T1,$comp248_T2);
+    return  $sec;
+  }
 }
 
 //Returns an array of Sessions objects for available labs for this specific lecture
-function getLabs($lecture)
+function getLabs($course)
 {
+  if ($course == "COMP232")
+  {
+    $comp232_Lab1 = new Session (2,"COMP232", "PQ", null, "F", array("J"), 1745, 2015, "SGW");
+    $comp232_Lab2 = new Session (2,"COMP232", "PZ", null, "F", array("W"), 1745, 2015, "SGW");
 
+    $sec = array ($comp232_Lab2, $comp232_Lab1);
+    return  $sec;
+  }
 }
 
 //Returns an array of Courses objects this user can take this semester from the remaining courses array of courses
---
+++ algorithm/Course.php
@@ -0,0 +1,143 @@
+<?php
+
+class Course
+{
+  private $courseName; // String
+  private $courseID;   // int
+  private $preReqs;    // array of Courses
+  private $coReqs;     // array of Courses
+  private $credits;    // int
+  private $pass;       // bool
+  private $engProfReq; // bool
+  private $priority;   // int
+
+  public function __construct($courseID, $courseName, $preReqs, $coReqs, $credits, $pass, $engProfReq)
+  {
+    $this->courseID = $courseID;
+    $this->courseName = $courseName;
+    $this->preReqs = $preReqs;
+    $this->coReqs = $coReqs;
+    $this->credits = $credits;
+    $this->pass = $pass;
+    $this->engProfReq = $engProfReq;
+    $this->priority = 0;
+  }
+
+  public function getCourseName()
+  {
+    return $this->courseName;
+  }
+
+  public function getCourseID()
+  {
+    return $this->courseID;
+  }
+
+  public function getPreReqs()
+  {
+    return $this->preReqs;
+  }
+
+  public function getCoReqs()
+  {
+    return $this->coReqs;
+  }
+
+  public function getCredits()
+  {
+    return $this->credits;
+  }
+
+  public function getPass()
+  {
+    return $this->pass;
+  }
+
+  public function getEngProfReq()
+  {
+    return $this->engProfReq;
+  }
+
+  public function getPriority()
+  {
+    return $this->priority;
+  }
+
+  public function setCourseID($id)
+  {
+    $this->courseID = $id;
+  }
+
+  public function setCourseName($name)
+  {
+    $this->courseName = $name;
+  }
+
+  public function setPriority($priority)
+  {
+    $this->priority = $priority;
+  }
+
+
+  public function calPriority ($courses)
+  {
+    deleteCourse($this, $courses);
+    $this->setPriority(0);
+
+    //echo "Checking $this->name <br>";
+
+    foreach ($courses as $c)
+    {
+      //echo "Checking $c->name <br>";
+      if ($c->getPreReqs() != null)
+      {
+
+        foreach ($c->getPreReqs() as $preReq)
+        {
+          if ($this->getCourseName() == $preReq->getCourseName() )
+          {
+          $this->setPriority(1+($c->calPriority($courses)));
+          //echo "$this->numDescendents <br>";
+          }
+        }
+      }
+      if ($c->getCoReqs() != null)
+      {
+        foreach ($c->getCoReqs() as $coReqs)
+        {
+          if ($this->getCourseName() == $coReqs->getCourseName())
+          {
+          //echo "entered the if <br>";
+          $this->setPriority(1+($c->calPriority($courses)));
+          }
+        }
+      }
+    }
+  return $this->priority;
+  }
+
+  public function updateAllPriority ($courses)
+  {
+    foreach ($courses as $c)
+      $c->calPriority($courses);
+  }
+
+  public function dispAllPriority ($courses)
+  {
+    foreach ($courses as $c)
+    {
+      echo $c->getCourseName() . "number of descendents' paths is $c->numDescendents <br>";
+    }
+  }
+//    public function dispLength($course)
+//    {
+//    $str=  (string)($this->coFinish - $this->coStart.);
+//    $arr1 = str_split($str, 3);
+//    foreach ($arr1 as $st) {
+//      echo "".$st." : ";
+//    }
+//    }
+
+}
+
+?>
