03b174a7503267cab0bf08b50ad3d5d7712d95c5
minhaoyu <46823793+minhaoyu@users.noreply.github.com>
2019-03-26 09:39:19 +0800

    modified for loop
    modified for loop

--- FrontEnd/backendInterface.php
+++ FrontEnd/backendInterface.php
@@ -29,25 +29,36 @@ function genNewSched ()
 		 }
 	}
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
 
 	$userSched = new UserSchedule("F", $numCoursesArr, $noClassesArr);
 
 	$userSched->genProgramSched('sebhani98@gmail.com');
 
-	$semInfo = array();
-	$semInfo[0]= array (array());
-	$semInfo[1]= array (array());
-	$semInfo[2]= array (array());
 
-	foreach ($userSched->getListOfSemesters()[0]->getLecs() as $lec)
+	$numSemesters = count($userSched->getListOfSemesters());
+
+	$semInfo = array();
+
+	for ($i =0;$i<=count($userSched->getListOfSemesters());$i++){
+
+	$semInfo[$i]= array (array());
+	//$semInfo[1]= array (array());
+	//$semInfo[2]= array (array());
+
+	foreach ($userSched->getListOfSemesters()[$i]->getLecs() as $lec)
 	{
 	  $courseInfo = array();
 	  $courseInfo['Course Name'] = $lec->getCourseName();
 	  $courseInfo['Credits'] = 3;
-	  array_push($semInfo[0],$courseInfo);
+	  array_push($semInfo[$i],$courseInfo);
 	}
-	$semInfo[0] = array_slice($semInfo[0],1);
+	$semInfo[$i] = array_slice($semInfo[$i],1);
 
+	var_dump($semInfo);
+}
+}
+/*
 
 	foreach ($userSched->getListOfSemesters()[1]->getLecs() as $lec)
 	{
@@ -69,9 +80,10 @@ function genNewSched ()
 
 	$_SESSION['semInfo'] = $semInfo;
 
-var_dump (count($semInfo));
+//var_dump (count($semInfo));
 
 }
+*/
 
 if( empty($_POST['submitID']) )                                   //A
 {
