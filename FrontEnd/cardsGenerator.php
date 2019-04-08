61e652250a5792baa9ceb1c20dcbf7ebfbc8385c
mohhef <mohhef@rocketmail.com>
2019-04-08 03:26:39 -0400

    Revert "Revert "Merge branch 'master' into loadingpageUserMo""
    This reverts commit 91e6ee129881777b56791af9c8ff6def1670b538.

--- FrontEnd/cardsGenerator.php
+++ FrontEnd/cardsGenerator.php
@@ -1,15 +1,24 @@
 <?php
+require_once __DIR__.'/../algorithm/Semester.php';
+
 if (session_status() != PHP_SESSION_ACTIVE)
+{
+	ob_start();
 	session_start();
+}
 function cardDisp($i)
 {
   $semInfoFE = $_SESSION ['semInfo'];
-  
+	$semYearFE = $_SESSION ['semYear'];
+	$semNameFE = $_SESSION ['semName'];
+
   echo
   '<a class="card card-body text-center height:400px" id="hello" href="weeklyschdulef.php?semester=';echo"$i";echo'"class="custom-card" style="background: #F8C471">'.
   '<thead>'.
   '<tr class="tableheader">'.
-  '<th><strong>Semester ';  echo $i + 1;
+  '<th><strong>';
+	echo 'Year '; echo $semYearFE[$i]; echo ', ';
+	echo $semNameFE[$i]; echo ' Semester';
   echo ' </th>'.
   '</tr></strong>'.
   '</thead>'.
--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 echo '<link href="css/fbBtmStyling.css" rel="stylesheet">';
 echo '<link href="css/style.css" rel="stylesheet">';
 
-if (session_status() == PHP_SESSION_ACTIVE)
-	session_destroy();
-
-session_start();
-
 
 //Page Info
 $page_name = "error 404 - Main page";
@@ -14,8 +9,19 @@ $page_keywords = "Error404";
 $page_description = "Error404";
 $page_author = "Error404";
 
+
 //Construct Page
+include 'FrontEnd/sessionfns.php';
 include 'PageBuilder/navbar.php';
+if (session_status() != PHP_SESSION_NONE)
+{
+  //session_destroy();
+  session_end();
+  unset($_SESSION);
+  $_SESSION = array();
+  ob_start();
+  session_start();
+}
 include 'FrontEnd/index.php';
 include 'PageBuilder/header.php';
 include 'PageBuilder/footer.php';
