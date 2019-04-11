ca2304eb142891e8a2447ba0e489d62eba0a303b
Osama <osama.qalam@hotmail.com>
2019-04-11 16:17:35 -0400

    Corrected the color of the constraints div

--- FrontEnd/Constraints.php
+++ FrontEnd/Constraints.php
@@ -121,13 +121,13 @@ background-image: -webkit-linear-gradient(top, rgba(230, 247, 255,9), rgba(230, 
     <h2 align="center">Add Costraints</h2>
 
 
-		<div style="background-color:#72c8e5"class="form-group p-3 mb-2 rounded">
+		<div style="background-color:#9cd8ed"class="form-group p-3 mb-2 rounded">
       <!-- <div class="center"> -->
 			<form name="add_name" id="add_name">
 				<table class="table table-borderless" id="dynamic_field">
 					 <tr>
 						<td>
-              <h2 style="color:#500c8c; font-size: 15px" align="left" style="color:#a3291b;">Choose Year</h2>
+              <h2 style="color:#000000; font-size: 15px" align="left" style="color:#000000;">Choose Year</h2>
               <!-- <div class="ceter"> -->
                 <select id= "listYear1",name="Years" >
                 <option value="1" selected>
@@ -190,7 +190,7 @@ background-image: -webkit-linear-gradient(top, rgba(230, 247, 255,9), rgba(230, 
 						</td>
 						<td>
                 <h2 style="font-size: 15px" align="center">Semester</h2>
-              <select id = "list1",name="Term">
+              <select id = "list1" align="center" name="Term">
 							<option value="Summer" selected>
                 <?php
 									if($_SESSION['dispEng'])
@@ -218,7 +218,7 @@ background-image: -webkit-linear-gradient(top, rgba(230, 247, 255,9), rgba(230, 
 							</select>
 
 						</td>
-						<td><h2 style="color:#500c8c; font-size: 15px" align="left">Maximum Classes This Semester </h2>
+						<td><h2 style="color:#000000; font-size: 15px" align="left">Maximum Classes This Semester </h2>
 
               <select name="Number" id="number1">
 							<option value="0" selected>0</option>
@@ -233,7 +233,7 @@ background-image: -webkit-linear-gradient(top, rgba(230, 247, 255,9), rgba(230, 
             <!-- <td>
               <p>  Minimum Credits This Semester </p> </td> -->
                   <td>
-                    <h2 style="color:#500c8c; font-size: 15px" align="left">
+                    <h2 style="color:#000000; font-size: 15px" align="left">
                       <?php
                         if ($_SESSION['dispEng'])
                         	echo 'Minimum credits this semester';
